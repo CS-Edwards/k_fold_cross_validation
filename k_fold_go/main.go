b03541e7460430fb78213c60e9d329185c564aed
CS-Edwards <candaces.edwards@gmail.com>
2023-06-01 14:57:04 -1000

    row validation and error handling

--- k_fold_go/main.go
+++ k_fold_go/main.go
@@ -1,8 +1,67 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
+
+type ValueError struct {
+	rowCountX uint
+	rowCountY uint
+	Msg       string
+}
+
+// Error() returns the error message associated with the ValueError.
+// It satisfies the error interface by implementing the Error() method.
+// Returns string 'Msg'
+func (e *ValueError) Error() string {
+	return e.Msg
+}
+
+// ValidateRow() raises error if input array lengths are not equal
+func ValidateRow(X [][]float64, y []float64) (bool, error) {
+	if len(X) != len(y) {
+
+		return false, &ValueError{
+			Msg:       "Input arrays X and y must have the same number of rows",
+			rowCountX: uint(len(X)),
+			rowCountY: uint(len(y)),
+		}
+
+	}
+
+	return true, nil
+
+}
+
+// Error wrapper for row validation
+// Calls ValidateRow() on input arrays and returns relevenat error message.
+// Returns ValueError or Unexpected error message.
+// Returns nil for no error
+func HandleRows(X [][]float64, y []float64) error {
+	_, err := ValidateRow(X, y)
+	if err != nil {
+		var valErr *ValueError
+		switch {
+		case errors.As(err, &valErr):
+			fmt.Printf("ValueError: X has %d rows, Y has %d rows: %s \n", valErr.rowCountX, valErr.rowCountY, valErr.Error())
+		default:
+			fmt.Printf("unexpected error: %s \n", err)
+		}
+
+		return fmt.Errorf("HandleRows(): failed: %w", err)
+	}
+
+	return nil
+}
+
+// k fold cross validation for X: 2D array, y: 1D array
+// TODO: implement k fold
+func kFoldCrossValidation(X [][]float64, y []float64, k uint) {
+
+	fmt.Printf("This had %v folds", k)
+
+}
 
 func main() {
 	var X = [][]float64{
@@ -18,33 +77,18 @@ func main() {
 		{1.1, 2.2, 3.3, 4.4},
 	}
 
-	var y = []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	//var y = []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	var y = []float64{1, 2.5}
 
 	fmt.Println("This is the main function for k-fold")
 
-	returned1, returned2 := validateRow(X, y)
-	fmt.Printf("\n validateRow(), returned %v, %v \n", returned1, returned2)
+	err := HandleRows(X, y)
 
-	kFoldCrossValidation(X, y, 5)
-
-}
-
-// k fold cross validation for X: 2D array, y: 1D array
-func kFoldCrossValidation(X [][]float64, y []float64, k uint) {
-
-	fmt.Printf("This had %v folds", k)
-
-}
-
-// Validate Row: raise error if input array lengths are not equal
-// TODO validateRow must raise ValueError if len(x) != len(y)
-func validateRow(X [][]float64, y []float64) (bool, error) {
-	if len(X) != len(y) {
-		fmt.Println("Raise Value Error")
-		return false, nil
-
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
 	}
 
-	return true, nil
+	kFoldCrossValidation(X, y, 5)
 
 }
